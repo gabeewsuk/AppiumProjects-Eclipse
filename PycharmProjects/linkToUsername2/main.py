0826215ed50a82599b7acddc32cd271e42a08315
Gabe Ewusk <gabeewsuk@gmail.com>
2021-03-11 19:26:52 -0700

    linkToUsername finished

--- PycharmProjects/linkToUsername2/main.py
+++ PycharmProjects/linkToUsername2/main.py
@@ -7,8 +7,7 @@
 
 #chrome_options = Options()
 
-
-
+import csv
 
 import pandas as pd
 
@@ -21,5 +20,36 @@ for index, row in df.iterrows():
     links.append(row['TikTok'])
 print(links)
 
+from selenium import webdriver
+
+#driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome("/usr/local/bin/chromedriver2", options = chrome_options)
+usernameTTList = []
+for x in links:
+    try:
+        driver.get(x)
+    except:
+        print("link does not work")
 
 
+    time.sleep(2)
+    try:
+        element = driver.find_element_by_xpath('//*[@id="main"]/div[2]/div[2]/div/header/div[1]/div[2]/h2')
+        usernameTT = element.text
+        usernameTTList.append(usernameTT)
+        print(usernameTT)
+    except:
+        print(x + "does not exist anymore")
+
+
+
+
+
+df = pd.DataFrame(usernameTTList)
+df.to_csv('UserNamesForSuggested.csv', index=False)
